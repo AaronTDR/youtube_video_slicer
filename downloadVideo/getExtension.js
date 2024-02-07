70b57e6c4979e8c6bc46cf3d9658cdba11c631d8
AaronTDR <aaron.tdr12w@gmail.com>
2024-02-07 06:34:36 -0600

    chore: Remove comments in 'getExtension' - #1

--- downloadVideo/getExtension.js
+++ downloadVideo/getExtension.js
@@ -1,36 +1,6 @@
 import fs from "fs";
 import path from "path";
 
-/* const directoryPath = "C:/users/aaron/downloads/test/";
-const fileName =
-  "Nature WhatsApp Status Video 30 Seconds| Nature Love Song Background 2022|4k";
-
-try {
-  const files = fs.readdirSync(directoryPath);
-  console.log("🚀 ~ files: LOL", files);
-  const file = files.find((file) => {
-    return file.includes(files);
-  });
-  console.log("🚀 ~ file: LOL", file);
-
-  const extension = path.extname(file);
-  console.log("🚀 ~ extension:", extension);
-
-    if (file) {
-    // El archivo se encontró
-    console.log(`El archivo: ${file} se encontró en ${directoryPath}`);
-    const extension = path.extname(file);
-    console.log("🚀 ~ extension:", extension);
-  } else {
-    // El archivo no se encontró
-    console.log(`El archivo: ${fileName} no se encontró en ${directoryPath}`);
-  }
-} catch (error) {
-  // Se produjo un error al leer el directorio
-  console.error(`Error al leer el directorio: ${error.message}`);
-
-} */
-
 const getExtension = (directoryPath, videoName) => {
   try {
     // Read the contents of the specified directory synchronously.
