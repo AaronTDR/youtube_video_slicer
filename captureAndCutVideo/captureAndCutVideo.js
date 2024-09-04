8c8ef6a85fdbf918d2da0ed3e88aa378011de9ed
Aaron Treviño Del Rio <aaron.tdr12w@gmail.com>
2024-09-04 13:46:09 -0600

    Fixes issues with timestamp accuracy

--- captureAndCutVideo/captureAndCutVideo.js
+++ captureAndCutVideo/captureAndCutVideo.js
@@ -24,17 +24,23 @@ const captureAndCutVideo = async (
     const durationClip = subtractTimestamps(end, start);
 
     const ffmpegProcess = cp.spawn("ffmpeg", [
-      "-ss",
-      start,
       "-i",
       videoPathWithFormat,
+      "-ss",
+      start, // Start time
       "-t",
-      durationClip,
-      "-map",
-      "0",
-      "-c",
-      "copy",
-      outputFilePath,
+      durationClip, // clip duration
+      // Lightweight recoding
+      "-c:v",
+      "libx264", // Encoder H.264
+      "-preset",
+      "veryfast", // Preset to speed up encoding
+      "-crf",
+      "23", // Quality: 0 is lossless, 23 is good quality, 51 is the lowest
+
+      "-c:a",
+      "aac", // AAC audio encoder
+      outputFilePath.replace(".webm", ".mp4"), // Output file
     ]);
 
     const promise = new Promise((resolve, reject) => {
