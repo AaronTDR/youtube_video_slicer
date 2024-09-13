f0e45db28ae86e7b255285483821af214344e34a
Aaron Treviño Del Rio <aaron.tdr12w@gmail.com>
2024-09-12 20:31:44 -0600

    Add local parameters

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
 export const config = {
-  ffmpeg_exe_path: "C:/ProgramData/chocolatey/lib/ffmpeg/tools/ffmpeg/bin/ffmpeg.exe",
-  ffprobe_exe_path: "C:/ProgramData/chocolatey/lib/ffmpeg/tools/ffmpeg/bin/ffprobe.exe",
-  workingFolderPath: "E:/projects/youtube_video_slicer/output/",
-  segmentsFolderPath: "E:/projects/youtube_video_slicer/output/segments/",
+  ffmpeg_exe_path: "ffmpeg",
+  ffprobe_exe_path: "ffprobe",
+  workingFolderPath: "C:/users/aaron/downloads/result/",
+  segmentsFolderPath: "C:/users/aaron/downloads/result/segments/",
   url: "https://www.youtube.com/watch?v=pDOHwsFkaFE",
   timestamps: [
-    { start: "00:00:07", end: "00:00:08" },
-    { start: "00:00:07", end: "00:00:08" },
-    { start: "00:00:07", end: "00:00:08" },
+    { start: "00:00:00", end: "00:00:03" },
+    { start: "00:00:07", end: "00:00:10" },
+    { start: "00:00:11", end: "00:00:14" },
   ],
   concurrencyLimit: 2,
   shortsConfig: {
-    shortThumbnailPath: 'E:/projects/youtube_video_slicer/output/short_1.jpg',
-    generateThumbnail: false,
+    shortThumbnailPath: "C:/users/aaron/downloads/result/short_1.png",
+    generateThumbnail: true,
   },
   isYoutubeShort: true,
 };
