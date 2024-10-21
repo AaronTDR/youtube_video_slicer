6d943ed3025648727d0f17c37b7fbce46016cca6
Aaron Treviño Del Rio <aaron.tdr12w@gmail.com>
2024-10-20 18:46:19 -0600

    refactor: Separate logic for Youtube short into its own function - #19

--- cutAndConcatenateVideo/concatenateSegments.js
+++ cutAndConcatenateVideo/concatenateSegments.js
@@ -1,14 +1,10 @@
 import fs from "fs";
+import youtubeShort from "./youtubeShort.js";
 import { config } from "../config.js";
 import { execP } from "../utils/functions.js";
 
-const {
-  ffmpeg_exe_path,
-  workingFolderPath,
-  targetFormat,
-  shortsConfig,
-  isYoutubeShort,
-} = config;
+const { ffmpeg_exe_path, workingFolderPath, targetFormat, isYoutubeShort } =
+  config;
 
 async function concatenateSegments(file, videoName) {
   try {
@@ -21,43 +17,7 @@ async function concatenateSegments(file, videoName) {
     await execP(concatCommand);
 
     if (isYoutubeShort === true) {
-      const blurredShortName = `${fileNameOutputWithoutExtension}_BLURRED_top_bottom${targetFormat}`;
-      const blurredShortFullPathname = `${workingFolderPath}${blurredShortName}`;
-
-      const command =
-        `${ffmpeg_exe_path} -i "${workingFolderPath}${fileNameOutputWithoutExtension}${targetFormat}" ` +
-        `-vf "split[original][copy];[copy]scale=-1:(ih*0.80)*(16/9)*(16/9),crop=w=ih*9/16,gblur=sigma=25[blurred];[blurred][original]overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2" ` +
-        `"${blurredShortFullPathname}"`;
-      await execP(command);
-
-      const compatibleShortName = `${fileNameOutputWithoutExtension}_COMPATIBLE_to_youtube_format.mp4`;
-      const compatibleShortFullPathName = `${workingFolderPath}${compatibleShortName}`;
-      const convertCommand =
-        `${ffmpeg_exe_path} -i "${blurredShortFullPathname}" ` +
-        `-c:v libx264 -crf 17 -preset fast ` +
-        `${compatibleShortFullPathName}`;
-      await execP(convertCommand);
-
-      if (shortsConfig && shortsConfig.generateThumbnail === true) {
-        if (
-          typeof shortsConfig.shortThumbnailPath !== "string" ||
-          shortsConfig.shortThumbnailPath.length === 0
-        ) {
-          const err =
-            "\n You need to specify a Thumbnail image for the Short! \n";
-          console.error(err);
-          throw err;
-        }
-
-        const imageVideoShortName = `${fileNameOutputWithoutExtension}_thumbnail_video.mp4`;
-        const imageVideoShortFullPathname = `${workingFolderPath}${imageVideoShortName}`;
-
-        const imageCommand =
-          `${ffmpeg_exe_path} -loop 1 -framerate 60 -t 5 -i "${shortsConfig.shortThumbnailPath}" ` +
-          `-f lavfi -i anullsrc=channel_layout=stereo:sample_rate=48000 -filter_complex "[0]scale=2560:4550:force_original_aspect_ratio=increase,crop=2560:4550,setsar=1,format=yuv420p[v]" ` +
-          `-map "[v]" -map 1 -c:v libx264 -c:a aac -shortest "${imageVideoShortFullPathname}"`;
-        await execP(imageCommand);
-      }
+      await youtubeShort(fileNameOutputWithoutExtension);
     }
   } catch (error) {
     console.error("File concat error: ", error);
